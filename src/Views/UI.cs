aa6b2cc4c768cf79e3281b6ac96f58393370cce3
rafael-aero <rafael@rafael.aero>
2022-01-24 10:44:03 +0100

    Update UI.cs

--- src/Views/UI.cs
+++ src/Views/UI.cs
@@ -5,13 +5,6 @@ namespace WordleSolver
 {
     public static class UI
     {
-        public static BackgroundArea CenteredCardWithBackground(IComponent content)
-        {
-            var card = Card(content).NoAnimation().Padding(32.px());
-            card.Render().style.maxHeight = "calc(100% - 32px)";
-            return BackgroundArea(card).S();
-        }
-
         public static BackgroundArea CenteredWithBackground(IComponent content) => BackgroundArea(content).S();
 
         public static IComponent GetLogo()
